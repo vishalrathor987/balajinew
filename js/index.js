b606d77e6c7a5014016d4199aa8b88cd5f2bd13b
vishal rathor <rathorvishal975@gmail.com>
2022-03-24 03:01:22 +0530

    balaji

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function clickimg(smallImg){
+    var fullImg = document.getElementById("bimg");
+    fullImg.src = smallImg.src
+}
+
+
+function pimg2img(smallImg){
+    var full2img = document.getElementById("bimg");
+    full2img.src = smallImg.src
+}
